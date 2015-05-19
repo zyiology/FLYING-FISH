ac7e5be8da50ef6e0a5a60d9ef9bc1717f09ff7c
CrazyCyfro <crazycyfro@gmail.com>
2015-05-19 16:15:50 +0800

    added chat activity

--- app/src/main/java/com/goldenhand/bleakfalls/flying_fish/CatApplication.java
+++ app/src/main/java/com/goldenhand/bleakfalls/flying_fish/CatApplication.java
@@ -5,16 +5,17 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-/**
- * Created by S9925872A on 5/12/2015.
- */
 public class CatApplication extends Application {
+    public static final String APPLICATION_ID = "a2jh49fEnFAsCzC9DouV1nBtDwWJWZtLDIYFehfZ";
+    public static final String CLIENT_KEY = "HvkqXU3W7nUj1e1KlMSDcqVNa4S4sssLRgV5aoFc";
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "a2jh49fEnFAsCzC9DouV1nBtDwWJWZtLDIYFehfZ", "HvkqXU3W7nUj1e1KlMSDcqVNa4S4sssLRgV5aoFc");
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
