8ac8256c892e342c97df95d2e537934df923cf92
CrazyCyfro <crazycyfro@gmail.com>
2015-06-05 01:26:47 +0800

    ADD FRIENDS DONE. DATABASE HAS BEEN REPOPULATED

--- app/src/main/java/com/goldenhand/bleakfalls/flying_fish/AddFriendListActivity.java
+++ app/src/main/java/com/goldenhand/bleakfalls/flying_fish/AddFriendListActivity.java
@@ -4,19 +4,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class AddFriendListActivity extends ActionBarActivity {
     public static final String USER_ID = "user id";
+    private static String mUserId;
+    private ParseUser currentUser;
+    private List<ParseUser> mFriends;
+
+    private ListView usernameLV;
 
     static List<ParseUser> mUserList;
 
@@ -25,6 +35,17 @@ public class AddFriendListActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend_list);
 
+        mUserId = getIntent().getStringExtra(USER_ID);
+
+        ParseQuery<ParseUser> selfQuery = ParseUser.getQuery();
+        selfQuery.getInBackground(mUserId, new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+                mFriends = parseUser.getList("friends");
+                currentUser = parseUser;
+            }
+        });
+
         ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
         userQuery.findInBackground(new FindCallback<ParseUser>() {
             @Override
@@ -32,8 +53,31 @@ public class AddFriendListActivity extends ActionBarActivity {
                 if (e == null) {
                     mUserList = list;
                     UserAdapter mUserAdapter = new UserAdapter(AddFriendListActivity.this, R.layout.activity_add_friend_list_item, mUserList);
-                    ListView usernameLV = (ListView) findViewById(R.id.username_list);
+                    usernameLV = (ListView) findViewById(R.id.username_list);
                     usernameLV.setAdapter(mUserAdapter);
+                    setUpOnClick();
+                }
+            }
+        });
+    }
+
+    private void setUpOnClick() {
+        usernameLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ParseUser selectedUser = (ParseUser) usernameLV.getItemAtPosition(position);
+
+                if (!mFriends.contains(selectedUser)) {
+
+                    mFriends.add(selectedUser);
+                    currentUser.put("friends",mFriends);
+                    currentUser.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+
+                        }
+                    });
+
                 }
             }
         });
