624bb2334243d3972fe14bcc519a74fd15d04947
CrazyCyfro <crazycyfro@gmail.com>
2015-06-04 23:04:07 +0800

    CAN VIEW USERS NOW

--- app/src/main/java/com/goldenhand/bleakfalls/flying_fish/UserAdapter.java
+++ app/src/main/java/com/goldenhand/bleakfalls/flying_fish/UserAdapter.java
@@ -1,5 +1,6 @@
 package com.goldenhand.bleakfalls.flying_fish;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,23 +16,23 @@ import java.util.List;
 public class UserAdapter extends ArrayAdapter<ParseUser> {
     List<ParseUser> mUsers;
     Context context;
+    int resource;
 
     public UserAdapter(Context context, int resource, List<ParseUser> mUsers) {
         super(context, resource, mUsers);
         this.mUsers = mUsers;
         this.context = context;
+        this.resource = resource;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final ViewHolder holder = new ViewHolder();
-
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_add_friend_list_item,parent,false);
-
-            holder.username = (TextView) convertView.findViewById(R.id.add_friend_username);
-            convertView.setTag(holder);
+            convertView = LayoutInflater.from(context).inflate(resource, parent, false);
         }
+        holder.username = (TextView) convertView.findViewById(R.id.add_friend_username);
+        convertView.setTag(holder);
 
         final ParseUser currentUser = mUsers.get(position);
         holder.username.setText(currentUser.getUsername());
