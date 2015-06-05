bd468df8315aee99c8d5de2bdda43756109efa5a
Zhi Yi <zyiology10000@gmail.com>
2015-06-05 11:16:09 +0800

    Fixed bug with newly registered users joining groups

--- app/src/main/java/com/goldenhand/bleakfalls/flying_fish/LoginActivity.java
+++ app/src/main/java/com/goldenhand/bleakfalls/flying_fish/LoginActivity.java
@@ -197,17 +197,16 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
                                 i.putExtra(REGISTERED_USER_ID, parseUser.getObjectId());
                                 startActivity(i);
                             } else {
-                                ParseUser newUser = new ParseUser();
+                                final ParseUser newUser = new ParseUser();
                                 //newUser.setEmail(mEmailView.getText().toString());
                                 newUser.setUsername(mUsernameView.getText().toString());
                                 newUser.setPassword(mPasswordView.getText().toString());
-                                final String objId = newUser.getObjectId();
                                 newUser.signUpInBackground(new SignUpCallback() {
                                     @Override
                                     public void done(ParseException e) {
                                         if (e == null) {
                                             Intent i = new Intent(getApplication(), FishActivity.class);
-                                            i.putExtra("USER_ID", objId);
+                                            i.putExtra(REGISTERED_USER_ID, newUser.getObjectId());
                                             startActivity(i);
                                         } else {
                                             //TODO UPDATE THIS
