06905f83d50672bd9115ee18001b3392a83ca708
CrazyCyfro <crazycyfro@gmail.com>
2015-06-30 18:10:45 +0800

    donez

--- app/src/main/java/com/goldenhand/bleakfalls/flying_fish/GroupDetailFragment.java
+++ app/src/main/java/com/goldenhand/bleakfalls/flying_fish/GroupDetailFragment.java
@@ -1,6 +1,8 @@
 package com.goldenhand.bleakfalls.flying_fish;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -29,12 +31,16 @@ import java.util.List;
 
 public class GroupDetailFragment extends Fragment {
     private final String ARG_SECTION_NUMBER = "section_number";
-    static List<ParseObject> mGroupList;
+    private ArrayList<ParseUser> mUserArrayList;
+    private GroupAdapter mGroupAdapter;
+
+    private ListView lv;
 
     private static String mUserId;
     private static boolean mIsRegistered;
     private static String mGroupId;
 
+    private AlertDialog alertDialog;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,7 +65,7 @@ public class GroupDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_fish_group_detail, container, false);
 
-        final ListView lv = (ListView) rootView.findViewById(R.id.user_list);
+        lv = (ListView) rootView.findViewById(R.id.user_list);
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Group");
         query.getInBackground(mGroupId, new GetCallback<ParseObject>() {
@@ -70,15 +76,16 @@ public class GroupDetailFragment extends Fragment {
                 userQuery.findInBackground(new FindCallback<ParseUser>() {
                     @Override
                     public void done(List<ParseUser> list, ParseException e) {
-                        ArrayList<ParseUser> mUserArrayList = new ArrayList<ParseUser>();
+                        mUserArrayList = new ArrayList<ParseUser>();
                         if (mUserIdArrayList!=null) {
                             for (int i = 0; i < list.size(); i++) {
                                 if (mUserIdArrayList.contains(list.get(i).getObjectId())) {
                                     mUserArrayList.add(list.get(i));
                                 }
                             }
-                            GroupAdapter mGroupAdapter = new GroupAdapter(getActivity(), R.layout.fragment_fish_group_detail, mUserArrayList);
+                            mGroupAdapter = new GroupAdapter(getActivity(), R.layout.fragment_fish_group_detail, mUserArrayList);
                             lv.setAdapter(mGroupAdapter);
+                            setUpGroupDetailOnClick();
                         }
                     }
                 });
@@ -86,6 +93,57 @@ public class GroupDetailFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    private void setUpGroupDetailOnClick() {
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final ParseUser selectedUser = (ParseUser) lv.getItemAtPosition(position);
+
+                ParseQuery<ParseObject> groupQuery = ParseQuery.getQuery("Group");
+                groupQuery.getInBackground(mGroupId, new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject parseObject, ParseException e) {
+                        if (parseObject.getString("admin").equals(mUserId)) {
+                            alertDialog = confirmUserKickDialog(selectedUser, parseObject);
+                            alertDialog.show();
+                        }
+                    }
+                });
+                return false;
+            }
+        });
+    }
+
+    private AlertDialog confirmUserKickDialog(final ParseUser user, final ParseObject group) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.delete_warning)
+                .setMessage(getText(R.string.kick_user)+" "+user.getUsername()+" from group?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        List<String> userIds = group.getList("UserIds");
+                        userIds.remove(user.getObjectId());
+                        group.put("UserIds", userIds);
+                        group.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                mUserArrayList.remove(user);
+                                mGroupAdapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .create();
+
     }
 
     private class GroupAdapter extends ArrayAdapter<ParseUser> {
